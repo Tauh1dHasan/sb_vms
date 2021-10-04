68c83ea43e01d56e15bd7cff315cb626790f50a1
RayhanZaman333 <rayhan.zaman333@gmail.com>
2021-10-04 10:44:17 +0600

    Reception Search Employee

--- resources/views/backend/partials/js.blade.php
+++ resources/views/backend/partials/js.blade.php
@@ -63,30 +63,6 @@
         });
     </script>
 
-    <script type="text/javascript">
-
-        $('#employee_id').select2({
-            placeholder: "Write Host Name",
-            allowClear: false,
-            ajax: {
-                url: '{{ route('visitor.search-employees') }}',
-                dataType: 'json',
-                delay: 250,
-                processResults: function (data) {
-                    return {
-                        results: $.map(data, function (item) {
-                            return {
-                                text: item.first_name+' '+item.last_name+' ('+item.designation+', '+item.department+')',
-                                id: item.employee_id
-                            }
-                        })
-                    };
-                },
-                cache: true
-            }
-        });
-    </script>
-
     <script> 
         $('form')
             .each(function(){
@@ -102,4 +78,6 @@
                 .prop('disabled', true)
         ;
     </script>
+
+    @include('backend.ajax.employeeSearch')
 
