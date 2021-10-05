030f9eeb6c4c2f9b6f416a9821936e411f5197e6
tauhid.hasan <tauhid.hasan@sebpo.com>
2021-10-05 17:33:43 +0600

    Reception and employee sidebar modified

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -214,7 +214,18 @@
                         <span>Declined List</span>
                     </div>
                 </a>
-            </li>   
+            </li>
+
+            <li class="menu menu-single">
+                <a href="{{ route('employee.profile') }}" data-active="false" class="menu-toggle">
+                    <div class="base-menu">
+                        <div class="base-icons">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
+                        </div>
+                        <span>Profile</span>
+                    </div>
+                </a>
+            </li>
 <!--------------------------------------------------  Employee Sidebar End  ----------------------------------------------------->   
 
 <!--------------------------------------------------  Reception Sidebar Start  ----------------------------------------------------->  
