5965c8940af11c91d1a477ff03cca6dc4086be89
tauhid.hasan <tauhid.hasan@sebpo.com>
2021-10-13 18:25:31 +0600

    working on admin panel visitor module

--- resources/views/backend/pages/admin/visitor/index.blade.php
+++ resources/views/backend/pages/admin/visitor/index.blade.php
@@ -77,7 +77,7 @@
                                                     @elseif($visitor->visitor_status == 1)
                                                         <span class="shadow-none badge badge-success">Approved</span>
                                                     @elseif($visitor->visitor_status == 2)
-                                                        <span class="shadow-none badge badge-danger">Declined</span>
+                                                        <span class="shadow-none badge badge-danger">Blocked</span>
                                                     @endif
                                                 </td>
 
@@ -90,14 +90,25 @@
                                                             <a class="dropdown-item action-view" href="{{ route('admin.visitor.show', $visitor->visitor_id) }}">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>View
                                                             </a>
-
                                                             <a class="dropdown-item action-edit" href="{{ route('admin.visitor.edit', $visitor->visitor_id) }}">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>Edit
                                                             </a>
-
-                                                            <a class="dropdown-item action-delete" onclick="return confirm('Are you sure want to delete?')" href="#">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>Delete
-                                                            </a>
+                                                            @if ($visitor->visitor_status == 0)
+                                                                <a class="dropdown-item action-view" href="#">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>Approve
+                                                                </a>
+                                                                <a class="dropdown-item action-delete" onclick="return confirm('Are you sure want to Block this visitor ?')" href="#">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>Block
+                                                                </a>
+                                                            @elseif($visitor->visitor_status == 1)
+                                                                <a class="dropdown-item action-delete" onclick="return confirm('Are you sure want to Block this visitor ?')" href="#">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>Block
+                                                                </a>
+                                                            @elseif($visitor->visitor_status == 2)
+                                                                <a class="dropdown-item action-view" href="#">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>Approve
+                                                                </a>
+                                                            @endif
                                                         </div>
                                                     </div>
                                                 </td>
